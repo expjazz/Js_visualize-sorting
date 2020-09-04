3f64a1288f7372c5c093343f685d3e1fe0c23acc
Expedito Andrade <expeditojazz@gmail.com>
2020-09-04 08:21:16 -0300

    add buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
-const button = (name) => {
+const button = (name, classes) => {
   const button = document.createElement('button');
   button.type = 'button';
+  button.classList = classes;
   button.innerText = name;
   return button;
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,12 @@ import button from './Button';
 
 const genNavbar = () => {
   const navBar = document.createElement('nav');
-  navBar.classList = 'bg-blue-400 h-32 w-100 flex ';
+  navBar.classList = 'bg-blue-400 h-32 w-100 flex pt-8 ';
   const sortDiv = document.createElement('div');
-  sortDiv.appendChild(button('bubble sort'));
+  const allSorts = ['Bubble Sort', 'Merge Sort', 'Quick Sort', 'Insert Sort'];
+  allSorts.forEach((sort) => {
+    sortDiv.appendChild(button(sort, 'mr-8'));
+  });
   navBar.appendChild(sortDiv);
   return navBar;
 };
