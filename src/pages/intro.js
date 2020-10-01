c966a2eb352f6cb5470a9ca3a59f00db8d0a18d6
Expedito Andrade <expeditojazz@gmail.com>
2020-10-01 19:55:58 -0300

    add buttons to intro page

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -4,9 +4,9 @@ import button from '../components/Button';
 const { root } = elements();
 const introGen = () => {
   const buttonsCont = document.createElement('div');
-  buttonsCont.classList = 'grid';
-  const sortingBtn = button('Sorting Algorithms', 'bg-blue-700 p-8 ');
-  const binarySearch = button('Binary Search', 'bg-blue-700 p-8 ');
+  buttonsCont.classList = 'grid justify-center gap-5';
+  const sortingBtn = button('Sorting Algorithms', 'bg-blue-700 px-8 w-9/12 ');
+  const binarySearch = button('Binary Search', 'bg-blue-700 px-8 w-9/12');
   buttonsCont.appendChild(sortingBtn);
   buttonsCont.appendChild(binarySearch);
   root.appendChild(buttonsCont);
