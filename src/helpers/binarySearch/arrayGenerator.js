4b3c9e4a2256137c84ea2dd8c596e901dacf2aa7
Expedito Andrade <expeditojazz@gmail.com>
2020-10-02 11:38:31 -0300

    add border to array

---
+++ src/helpers/binarySearch/arrayGenerator.js
@@ -0,0 +1,6 @@
+export default function arrayGenerator() {
+  const container = document.createElement('div');
+  container.classList.add('arrayNumber');
+  container.innerText = 'Test';
+  return container;
+}
--- src/pages/BinarySearch.js
+++ src/pages/BinarySearch.js
@@ -1,9 +1,11 @@
 import navbar from '../components/binarySearch/navBar';
 import elements from '../components/elements';
+import arrayGen from '../helpers/binarySearch/arrayGenerator';
 
 const pageBinarySearch = () => {
   const { root } = elements();
   root.appendChild(navbar());
+  root.appendChild(arrayGen());
 };
 
 export default pageBinarySearch;
