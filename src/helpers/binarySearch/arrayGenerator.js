ccc8522c4294c943e07662c067bf486341929e92
Expedito Andrade <expeditojazz@gmail.com>
2020-10-02 11:48:17 -0300

    add array generator

--- src/helpers/binarySearch/arrayGenerator.js
+++ src/helpers/binarySearch/arrayGenerator.js
@@ -1,6 +1,13 @@
 export default function arrayGenerator() {
   const container = document.createElement('div');
-  container.classList.add('arrayNumber');
-  container.innerText = 'Test';
+  container.classList = 'arrayNumber flex p-8 m-8';
+  const array = [];
+  for (let i = 0; i < 30; i++) {
+    const num = Math.floor(Math.random() * 100);
+    array.push(num);
+  }
+  array.sort((a, b) => a - b);
+  container.innerHTML = array.map((num) => `<p class="flex-1">${num}</p>`);
+
   return container;
 }
